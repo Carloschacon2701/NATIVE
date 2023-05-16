03216760e31b61d5f7023637dd319481f5631dfb
Carlos Chacon <carlosdchaconmendez@gmail.com>
2023-05-16 17:27:38 -0400

    commit

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -14,12 +14,51 @@ import { icons, SIZES } from "../../../constants";
 
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full-time");
+
+  const jobTypes = ["Full-Time", "Part-Time", "Contractor"];
 
   return (
     <View>
       <View style={styles.container}>
         <Text style={styles.userName}>Hello Carlos</Text>
         <Text style={styles.welcomeMessage}>Fin your perfect Job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
+          />
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onChange={() => {}}>
+          <Image
+            source={icons.search}
+            style={styles.searchBtnImage}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                router.push(`/search/${item}`);
+                setActiveJobType(item);
+              }}
+            >
+              <Text style={styles.tab(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
       </View>
     </View>
   );
---
+++ hooks/useFetch.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": "92b0bdd1a6msh71ea0c86b4224d5p13a154jsnf495213c74fa",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setLoading(true);
+    fetchData();
+  };
+
+  return { data, loading, error, refetch };
+};
+
+export default useFetch;
