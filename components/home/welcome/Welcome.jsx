7ea25d8f4ef6b1538a1ed84899a6a7432115abc4
Carloschacon2701 <carlosdchaconmendez@gmail.com>
2023-05-15 20:22:22 -0400

    first commit

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+
+import styles from "./welcome.style";
+import { useRouter } from "expo-router";
+import { icons, SIZES } from "../../../constants";
+
+const Welcome = () => {
+  const router = useRouter();
+
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Carlos</Text>
+        <Text style={styles.welcomeMessage}>Fin your perfect Job</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
